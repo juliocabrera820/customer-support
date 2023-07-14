b06ab5cd79d00940866183d3a21f3c7a82820e65
Cristian Chable <cchable@reservamos.mx>
2023-07-13 23:42:39 -0600

    dotenv and rspec gems added

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,12 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
+
+HOSTNAME = ENV['HOSTNAME']
+
 module CustomerSupport
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
