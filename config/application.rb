6afb1cbc48deecd9a037220dd23b5fa42527811a
Cristian Chable <cchable@reservamos.mx>
2023-07-14 20:36:59 -0600

    CR comments

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,6 @@ if ['development', 'test'].include? ENV['RAILS_ENV']
   Dotenv::Railtie.load
 end
 
-HOSTNAME = ENV['HOSTNAME']
-
 module CustomerSupport
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
