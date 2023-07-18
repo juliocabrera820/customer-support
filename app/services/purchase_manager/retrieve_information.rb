442035062a02420710644dc1a6e8f00f157d7f62
Cristian Chable <cchable@reservamos.mx>
2023-07-17 22:24:10 -0600

    CR comments

--- app/services/purchase_manager/retrieve_information.rb
+++ app/services/purchase_manager/retrieve_information.rb
@@ -5,21 +5,27 @@ module PurchaseManager
   class RetrieveInformation < ApplicationService
     attr_accessor :purchase_token
 
+    HTTP_OK_CODE = 200
+
     def initialize(purchase_token)
       @purchase_token = purchase_token
     end
 
     def call
-      response = RestClient.get(url, {})
-      response.code == 200 ? JSON.parse(response.body) : nil
+      response = RestClient.get(purchase_url, {})
+      response.code == HTTP_OK_CODE ? JSON.parse(response.body) : nil
     rescue RestClient::ExceptionWithResponse
       nil
     end
 
     private
 
-    def url
-      "#{ENV['PURCHASE_API_URL']}/api/v2/purchases/#{purchase_token}"
+    def base_url
+      "#{ENV['PURCHASE_API_URL']}/api/v2"
+    end
+
+    def purchase_url
+      "#{base_url}/purchases/#{purchase_token}"
     end
   end
 end
--- spec/services/purchase_manager/retrieve_information_spec.rb
+++ spec/services/purchase_manager/retrieve_information_spec.rb
@@ -4,16 +4,24 @@ require 'rails_helper'
 
 describe PurchaseManager::RetrieveInformation, type: :model do
   let(:token) { 'BDCxQxsKSZ8' }
-  let(:success_response) { File.read("#{Rails.root}/spec/fixtures/success_purchase_api_response.json") }
+  let(:success_response) { File.read("#{Rails.root}/spec/fixtures/purchase_manager/success_retrieve_information.json") }
 
-  it 'should be a PurchaseClient' do
-    expect(described_class).to eq(PurchaseManager::RetrieveInformation)
+  context 'with a purchase complete' do
+    it 'return status completed' do
+      stub_request(:get, "#{ENV['PURCHASE_API_URL']}/api/v2/purchases/#{token}")
+        .to_return(body: success_response, status: 200)
+      response = described_class.call(token)
+      expect(response.fetch('purchase').fetch('state')).to eq('completed')
+      expect(response.fetch('purchase').fetch('token')).to eq(token)
+    end
   end
 
-  it 'should response with valid information' do
-    stub_request(:get, "#{ENV['PURCHASE_API_URL']}/api/v2/purchases/#{token}")
-      .to_return(body: success_response, status: 200)
-    response = described_class.call(token)
-    expect(response.fetch('purchase').fetch('token')).to eq(token)
+  context 'purchase not found' do
+    it 'return nil' do
+      stub_request(:get, "#{ENV['PURCHASE_API_URL']}/api/v2/purchases/#{token}")
+        .to_return(body: '', status: 404)
+      response = described_class.call(token)
+      expect(response).to be_nil
+    end
   end
 end
